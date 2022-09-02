6dfab7793819daa963aa8c496ab83c27ce1fc320
Ex <atomic.theck@gmail.com>
2022-09-02 16:45:33 +0200

    Initial Commit

--- src/com/java/framework/localization/Category.java
+++ src/com/java/framework/localization/Category.java
@@ -1,6 +1,10 @@
 package com.java.framework.localization;
 
+import java.util.ArrayList;
+
 public class Category {
+    private final ArrayList<Entry> categoryEntries = new ArrayList<>();
+
     private final String name;
 
     Category(String name) {
@@ -9,5 +13,28 @@ public class Category {
 
     public String getName() {
         return this.name;
+    }
+
+    public Entry createEntry(String entryName, String defaultValue) throws Exception {
+        if (!this.entryExist(entryName)) {
+            Entry newEntry = new Entry(entryName, defaultValue);
+            categoryEntries.add(newEntry);
+            return newEntry;
+        } else throw new IllegalArgumentException();
+    }
+
+    public Entry getEntry(String entryName) {
+        return this.categoryEntries.get(this.getEntryIndex(entryName));
+    }
+
+    public boolean entryExist(String entryName) {
+        return getEntryIndex(entryName) >= 0;
+    }
+
+    private int getEntryIndex(String entryName) {
+        for (int i = 0; i < categoryEntries.size(); i++) {
+            if (categoryEntries.get(i).getName() == entryName) return i;
+        }
+        return -1;
     }
 }
--- src/com/java/framework/localization/Manager.java
+++ src/com/java/framework/localization/Manager.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 public class Manager {
     private final ArrayList<Category> localizationCategories = new ArrayList<>();
 
+    public Category createCategory(String categoryName) throws Exception {
+        if (!this.categoryExist(categoryName)) {
+            Category newCategory = new Category(categoryName);
+            localizationCategories.add(newCategory);
+            return newCategory;
+        } else throw new IllegalArgumentException();
+    }
+
     public Category getCategory(String categoryName) {
         return this.localizationCategories.get(this.getCategoryIndex(categoryName));
+    }
+
+    public boolean categoryExist(String categoryName) {
+        return getCategoryIndex(categoryName) >= 0;
     }
 
     private int getCategoryIndex(String categoryName) {
